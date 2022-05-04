7534373f481ed6ac909873e4ce71e8e1bf4e54bd
Rebecca Mansell <rebeccakatemansell@gmail.com>
2022-05-04 18:26:13 +1000

    Progress on loops exercises

--- loops/loops_exercise.py
+++ loops/loops_exercise.py
@@ -7,3 +7,12 @@ user_number = int(input("Enter a number: "))
 for i in range(1, user_number + 1):
   # Print calculation and results
   print(f"{user_number} * {i} = {user_number * i}")
+
+# Q3)
+
+random_numbers = [3, 5, 9, 1]
+
+for number in random_numbers:
+  sum = 0
+  sum = sum + number
+  print(sum)
