5ac93c3d8b7ee444c6614b4d80a8ded0450b7c1b
Rebecca Mansell <rebeccakatemansell@gmail.com>
2022-05-04 18:38:20 +1000

    All but Q2 finished

--- loops/loops_exercise.py
+++ loops/loops_exercise.py
@@ -1,18 +1,29 @@
 # Q1)
 
-# Ask user for a number
 user_number = int(input("Enter a number: "))
 
-# Create list of numbers, ending with number user specified
 for i in range(1, user_number + 1):
-  # Print calculation and results
   print(f"{user_number} * {i} = {user_number * i}")
 
 # Q3)
 
 random_numbers = [3, 5, 9, 1]
+sum = 0
 
 for number in random_numbers:
-  sum = 0
   sum = sum + number
-  print(sum)
+  
+print(sum)
+
+# Q4)
+
+mailing_list = [
+  ["Chilli", "chilli@thechihuahua.com"],
+  ["Roary", "roary@moth.catchers"],
+  ["Remus", "remus@kapers.dog"],
+  ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+  ["Ivy", "noreply@goldendreamers.xyz"],
+]
+
+for items in mailing_list:
+  print(f"{items[0]}: {items[1]}")
