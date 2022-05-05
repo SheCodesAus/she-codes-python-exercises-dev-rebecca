c698ce8acf08ba7c3d2641e76f17ea918bbfd78e
Rebecca Mansell <rebeccakatemansell@gmail.com>
2022-05-05 17:55:28 +1000

    Update while_loops_exercises.py
    uncommented work

--- loops/while_loops_exercises.py
+++ loops/while_loops_exercises.py
@@ -1,23 +1,23 @@
 # Q1)
 # I acknowledge that I probably did this in the worst/longest possible way
 
-# number = "not nothing"
-# list = []
+number = "not nothing"
+list = []
 
-# while number != "":
-#   number = input("Enter a number: ")
-#   list.append(number)
+while number != "":
+  number = input("Enter a number: ")
+  list.append(number)
 
-# list.pop()
-# list = [int(i) for i in list] # List comprehension - recreating the list with int instead of str
-# print(sum(list))
+list.pop()
+list = [int(i) for i in list] # List comprehension - recreating the list with int instead of str
+print(sum(list))
 
 # Q2)
 
 # Not sure how to do this with a while loop instead of for loop
-# number = int(input("Enter a number: "))
-# for num in range(1, number, 2):
-#   print(num)
+number = int(input("Enter a number: "))
+for num in range(1, number, 2):
+  print(num)
 
 # Q3)
 
@@ -33,4 +33,3 @@ while guess != number:
     print("Too low!")
   if guess == number:
     print("Correct!")
-    
